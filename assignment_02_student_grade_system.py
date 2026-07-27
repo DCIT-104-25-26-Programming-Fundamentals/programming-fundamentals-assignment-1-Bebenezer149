92190ecdcf1a8c8d4a5fee8ca5a1ac8c28bf68ff
Ebenezer Baidoo <bebenezer149@gmail.com>
2026-07-27 12:28:45 +0000

    Done with second assignment

--- assignment_02_student_grade_system.py
+++ assignment_02_student_grade_system.py
@@ -45,7 +45,7 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
-num=int(input("Enter student score: "))
+num=float(input("Enter student score: "))
 
 
 if(80 <= num and num <= 100):
